c0786c0c61de0269cae3185810854b13bb79841a
rahulshinde <rshinde@mica.edu>
2015-03-27 16:32:02 -0400

    adding javascript

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,38 @@
-$(document).ready( function () {
-	$("#form-selection").on("change", function() {
-	    var id = $(this).val() + "_content";
-	    $("#" + id).show().siblings().hide()
-	})
+var SITE = SITE || {};
+
+SITE.accordian = {
+	init: function() {
+		enquire.register('screen and (max-width: 599px)', {
+			match : function() {
+				alert('mobile');
+			},
+
+			unmatch : function() {
+				alert('desktop')
+			}
+		});
+	}
+};
+
+SITE.features = {
+	init: function() {
+		var features = $('body').data('features');
+		var featuresArray = [];
+
+		if(features) {
+			featuresArray = features.split(' ')
+
+			for(var i = 0; featuresArray.length; i++) {
+				var func = featuresArray[i];
+
+				if(SITE[func] && typeof SITE[func].init === 'function') {
+					SITE[func].init();
+				}
+			}
+		}
+	}
+};
+
+$(function () {
+	SITE.features.init();
 });
